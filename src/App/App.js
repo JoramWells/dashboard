5baedabfb57d95dc8311dadd78c3873c5d724589
bramuel <jorammanoah1@gmail.com>
2021-02-23 07:32:03 -0800

    Final commit

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,8 @@ import '../App.css';
 import SideMenu from '../components/SideMenu';
 import {CssBaseline, makeStyles} from "@material-ui/core"
 import Header from '../components/Header';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone'
+import PageHeader from '../components/PageHeader';
 
 const useStyles = makeStyles({
   appMain:{
@@ -17,6 +19,12 @@ function App() {
     <SideMenu />
     <div className={classes.appMain}>
     <Header />
+    <PageHeader
+    title="Page header"
+    
+    subTitle="Page description"
+    icons={<PeopleOutlineTwoToneIcon />}
+     />
     </div>
     <CssBaseline />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,43 @@
 import React from 'react'
-import {AppBar, Toolbar, Grid, InputBase, IconButton, Badge} from '@material-ui/core'
+import {AppBar, Toolbar, Grid, InputBase, IconButton, Badge, makeStyles} from '@material-ui/core'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
 import PowerSettingNewIcon from '@material-ui/icons/PowerSettingsNew'
+import SearchIcon from '@material-ui/icons/Search'
 
+const useStyles = makeStyles({
+    root:{
+        backgroundColor:"white",
+        transform:'translateZ(0)'
+    },
+    searchInput:{
+        opacity:'0.6',
+        padding:'0px 8px',
+        fontSize:'0.8rem',
+        '&:hover':{
+            backgroundColor:"#f2f2f2"
+        },
+        '& .MuiSvgIcon-root':{
+            marginRight:'8px'
+        }
+    }
+})
 
 export default function Header() {
+    const classes = useStyles();
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.root}>
             <Toolbar>
-                <Grid container>
+                <Grid container
+                alignItems="center"
+                >
                     <Grid item>
-                        jdksjkdj
-                        <InputBase />
+                        
+                        <InputBase 
+                        placeholder="Search course"
+                        className={classes.searchInput}
+                        startAdornment={< SearchIcon fontSize="sm"/>}
+                        />
 
                     </Grid>
                     <Grid item sm></Grid>
